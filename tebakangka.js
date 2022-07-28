48d9abf71107ec3d4f6a4c00a8b1eb7d8654a8b9
alifsyahakbar <alifsyah789@gmail.com>
2022-07-28 21:01:15 +0700

    upload

---
+++ tebakangka.js
@@ -0,0 +1,44 @@
+function main() {
+   var nama = prompt("Masukan nama anda :");
+
+   if(nama === 0) {
+      prompt("Masukan nama anda :");
+   }
+
+   let ulang = true;
+   while(ulang) {
+
+      let kesempatan = 3;
+      let nilai = Math.floor(Math.random()*10+1);
+      let hasil = '';
+
+      while(kesempatan >= 1) {
+         alert('Hi! ' + nama.charAt(0).toUpperCase() + nama.slice(1) + '🙌' + '\nPermainan dengan ' + kesempatan + ' kesempatan.');
+         let player = parseInt(prompt("Tebak Angka 1 - 10"));
+         if (player === nilai) {
+            hasil = 'BENAR';
+            alert('Anda ' + hasil + ' angka yang anda tebak adalah ' + nilai);
+            break;
+         } else if(player < nilai) {
+            hasil = 'RENDAH';
+            kesempatan -= 1;
+            alert('Nilai anda terlalu ' + hasil + '. Kesempatan anda sisa ' + kesempatan );
+         } else if(player > nilai) {
+            hasil = 'TINGGI';
+            kesempatan -= 1;
+            alert('Nilai anda terlalu ' + hasil + '. Kesempatan anda sisa ' + kesempatan);
+         } else {
+            break;
+         }
+         
+         if(kesempatan === 0) {
+            alert('Kesempatan telah habis, ANDA GAGAL! . Nilai yg dicari adalah ' + nilai);
+         }
+      }
+      ulang = confirm('Ulang Lagi?');
+
+
+}
+  swal("Terimakasih " + nama.charAt(0).toUpperCase() + nama.slice(1) + " !", "Lain waktu main lagi yaa!" , "success");
+}
+
